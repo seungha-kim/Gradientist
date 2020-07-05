2fa36788a6a2172396c8edc3d5e4aa59b866c224
Kim Seungha <seungha.me@gmail.com>
2020-07-05 13:40:22 +0900

    Initial Commit

---
+++ Gradientist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gradientist
+//
+//  Created by Kim Seungha on 2020/07/05.
+//  Copyright © 2020 Kim Seungha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
